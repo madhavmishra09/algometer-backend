6dccc3a814d4e0767b7b272962c30e0591aabe48
Madhav Mishra <madhavmishra083@gmail.com>
2026-03-17 17:50:39 +0530

    Fixed the bug

--- src/analyzer/binarySearchAnalyzer.js
+++ src/analyzer/binarySearchAnalyzer.js
@@ -1,27 +1,42 @@
-const walk=require("acorn-walk");
-function analyzeBinarySearch(ast){
-    let midDetected=false;
-    let boundaryUpdate=false;
-    let loopDetected=false;
-    walk.simple(ast,{
-        WhileStatement(){
-            loopDetected=true;
-        },
-        VariableDeclaration(node){
-            if(node.id.name==="mid" && node.init && node.init.type==="BinaryExpression"){
-                midDetected=true;
-            }
-        },
-        AssignmentExpression(node){
-            if(node.right && node.right.type==="BinaryExpression" && node.right.left && node.right.left.name==="mid"){
-                boundaryUpdate=true;
-            }
-        }
-    });
-    if(loopDetected && midDetected && boundaryUpdate){
-        return true;
+const walk = require("acorn-walk");
+const getName = require("../utils/getName");
+
+function analyzeBinarySearch(ast) {
+
+  let midDetected = false;
+  let boundaryUpdate = false;
+  let loopDetected = false;
+
+  walk.simple(ast, {
+
+    WhileStatement() {
+      loopDetected = true;
+    },
+
+    VariableDeclarator(node) {
+
+      if (
+        node.id &&
+        node.id.name === "mid"
+      ) {
+        midDetected = true;
+      }
+
+    },
+
+    AssignmentExpression(node) {
+
+      const leftName = getName(node.left);
+
+      if (leftName === "l" || leftName === "r") {
+        boundaryUpdate = true;
+      }
+
     }
-    return false;
+
+  });
+
+  return loopDetected && midDetected && boundaryUpdate;
 }
 
-module.exports=analyzeBinarySearch;
+module.exports = analyzeBinarySearch;
